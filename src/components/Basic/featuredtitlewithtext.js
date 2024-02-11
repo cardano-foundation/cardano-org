b7e142c2b5fc282a6e390aea3c6fd414f427cd36
Tommy Kammerer <tk@digitalsirup.com>
2024-02-11 13:52:40 +0100

    Update components

--- src/components/Basic/featuredtitlewithtext.js
+++ src/components/Basic/featuredtitlewithtext.js
@@ -1,26 +1,37 @@
 import React from "react";
 import clsx from "clsx";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Link from '@docusaurus/Link';
 //import styles from "./component-name.module.css"; // in case you need local css for this component
 //import { makeStyles, Typography, Box } from '@material-ui/core'; // in case you need some material-ui
 
 //
-// This component shows a simple header with some text below
- 
-function TitleWithText({ title, description }) {
+// This component:
+// shows a header on the left, with with some text on the right,
+// a tagline below and a call to action button below the tagline
+
+function FeaturedTitleWithText({ title, description, quote, buttonLabel, buttonLink }) {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
 
+  //const classes = useStyles();
+
   return (
     <div className="container">
-      <h2 className="red-text">
+      <h2 className="black-text">
       {title}
       </h2>
       <p className="black-text">
       {description}
       </p>
+      <h4 className="red-text">
+      {quote}
+      </h4>
+      <Link className="button button--primary" to={buttonLink}>
+      {buttonLabel}
+    </Link>
     </div> 
   );
 };
 
-export default TitleWithText;
+export default FeaturedTitleWithText;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import SiteHero from "@site/src/components/Basic/sitehero";
 import TitleWithText from "@site/src/components/Basic/titlewithtext";
-
+import FeaturedTitleWithText from "@site/src/components/Basic/featuredtitlewithtext";
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
@@ -17,16 +17,19 @@ export default function Home() {
         bannerType ='default'
       />
       <main>
-        <TitleWithText 
+        
+        <FeaturedTitleWithText
           title='Our World Is Changing. Together, We Can Change It For The Better.'
-          description='Cardano is a proof-of-stake blockchain platform: the first to be founded on peer-reviewed 
-          research and developed through evidence-based methods. It combines pioneering technologies 
-          to provide unparalleled security and sustainability to decentralized applications, systems, and societies.
+          description='Cardano is a proof-of-stake blockchain platform: the first to be founded on peer-reviewed research and developed through evidence-based methods. It combines pioneering technologies to provide unparalleled security and sustainability to decentralized applications, systems, and societies.
 
-          With a leading team of engineers, Cardano exists to redistribute power from unaccountable structures to 
-          the margins – to individuals – and be an enabling force for positive change and progress.'
+          With a leading team of engineers, Cardano exists to redistribute power from unaccountable structures to the margins – to individuals – and be an enabling force for positive change and progress.'
+          quote='A History Of Impossible, Made Possible'
+          buttonLabel='Use Cases'
+          buttonLink='/enterprise'
         />
+
         <HomepageFeatures />
+        
       </main>
     </Layout>
   );
