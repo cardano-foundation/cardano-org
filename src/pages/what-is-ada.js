86eb016a466b24d995126cefe6743e3da395a740
Tommy Kammerer <tk@digitalsirup.com>
2024-02-11 22:38:07 +0100

    add what is ada component

--- src/pages/what-is-ada.js
+++ src/pages/what-is-ada.js
@@ -2,6 +2,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import SiteHero from "@site/src/components/SiteHero";
+import WhatIsAda from '../components/WhatIsAda';
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
@@ -15,7 +16,29 @@ export default function Home() {
         bannerType ='ada'
       />
       <main>
-        <HomepageFeatures />
+        <WhatIsAda 
+          headline='What is ada?' 
+          title='Ada Is The Native Token Of Cardano'
+          description={[
+            'It is named after Ada Lovelace: a 19th-century mathematician who is recognized as the first \
+            computer programmer, and is the daughter of the poet Lord Byron.',
+            <br key="line1" />,
+            <br key="line2" />,
+            'Ada is a digital currency. Any user, located anywhere in the world, can use ada as a secure \
+            exchange of value – without requiring a third party to mediate the exchange. Every transaction \
+            is permanently, securely, and transparently recorded on the Cardano blockchain.',
+            <br key="line3" />,
+            <br key="line4" />,
+            'Every ada holder also holds a stake in the Cardano network. Ada stored in a wallet can be delegated \
+            to a stake pool to earn rewards – to participate in the successful running of the network – or found \
+            and run your own stake pool to increase the pool\'s likelihood of receiving rewards. In time, ada will \
+            also be usable for a variety of applications and services on the Cardano platform.',
+            <br key="line5" />,
+            <br key="line6" />,
+          ]}
+          quote='What can I do with ada?'
+          
+        />
       </main>
     </Layout>
   );
