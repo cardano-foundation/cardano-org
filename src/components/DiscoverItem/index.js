234ddfd4193225044a4fa84cc50bc9e89ac00769
Tommy Kammerer <tk@digitalsirup.com>
2024-02-21 13:42:19 +0100

    add discover item

---
+++ src/components/DiscoverItem/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import clsx from "clsx";
+import styles from './styles.module.css';
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Link from '@docusaurus/Link';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import { parseTextWithLinks } from '@site/src/utils/textUtils';
+
+
+//
+// This component:
+// shows an image on the left and some text to the right, title, subtitle are optional
+
+function DiscoverItem({ imageName, title, subtitle, text }) {
+  
+  // Construct the image URL using the imageName prop, we may want to handle image load errors in the future
+  const imageUrl = useBaseUrl(`/img/discover/${imageName}.jpg`);
+  
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageWrap}>
+        <img src={imageUrl} alt={imageName} />
+      </div>
+      <div className={styles.textWrap}>
+        { title && (
+        <h1 class="headingDot">{title}</h1>
+        )}
+        { subtitle && (
+        <h3>{subtitle}</h3>
+        )}
+        { text && (
+        <p>{text}</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default DiscoverItem;
+
--- src/pages/discover-cardano.js
+++ src/pages/discover-cardano.js
@@ -7,6 +7,7 @@ import SiteHero from '@site/src/components/SiteHero';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
+import DiscoverItem from '../components/DiscoverItem';
 
  
 
@@ -23,7 +24,23 @@ export default function Home() {
         bannerType ='starburst'
       />
       <main>
-         
+         <DiscoverItem
+          imageName={'people'}
+          title={'People'}
+          subtitle={'Working together, we achieve more for the many.'}
+          text={[
+            'Cardano is built by a decentralized community of scientists, engineers, and thought leaders united in a common \
+          purpose: to create a technology platform that will ignite the positive change the world needs. We believe the future \
+          should not be defined by the past, and that more is possible - and, through technology, can be made possible for all. \
+          We measure the worth of a task not by its challenge, but by its results.',
+          <br key='line1'/>,
+          <br key='line2'/>,
+          'Every ada holder also holds a stake in the Cardano network. Ada stored in a wallet can be delegated to a stake pool \
+          to earn rewards – to participate in the successful running of the network – or pledged to a stake pool to increase the \
+          pool’s likelihood of receiving rewards. In time, ada will also be usable for a variety of applications and services on \
+          the Cardano platform.'
+          ]}
+         />
       </main>
     </Layout>
   );
