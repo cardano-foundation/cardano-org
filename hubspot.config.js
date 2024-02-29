11f2144b07dbc981f0f75ed37fb6ce7c69842004
Tommy Kammerer <tk@digitalsirup.com>
2024-02-29 08:58:00 +0100

    Move HubSpot config data into config file

---
+++ hubspot.config.js
@@ -0,0 +1,19 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+// Configuration for HubSpot Form
+const HubSpotConfig = {
+
+  //  the form on http://localhost:3000/contact
+  contactForm: {
+    portalId: '7759219',
+    formId: '87e356e8-908d-4d11-93ba-ecdcd8227c4f',
+    region: 'na1',
+    target: '#hubspotForm'
+  }
+};
+
+export default HubSpotConfig;
