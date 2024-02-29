0b253f21d4c8a086e68ea6a1b293f21084d7dee0
Tommy Kammerer <tk@digitalsirup.com>
2024-02-29 20:58:56 +0100

    Make call to action boxes more versatile

--- src/components/Layout/CtaTwoColumn/index.js
+++ src/components/Layout/CtaTwoColumn/index.js
@@ -8,11 +8,22 @@ import styles from "./styles.module.css";
 // This component shows some text with a title (optional) to the left (optional)
 // and a call to action button on the right
 
-export default function CtaTwoColumn({ title, text, buttonLabel, buttonLink }) {
+export default function CtaTwoColumn({
+  leftTitle,
+  leftText,
+  leftButtonLabel,
+  leftButtonLink,
+  leftHeadingDot,
+  rightTitle,
+  rightText,
+  rightButtonLabel,
+  rightButtonLink,
+  rightHeadingDot,
+}) {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
 
-   const renderText = (text) => {
+  const renderText = (text) => {
     // Check if text is an array
     if (Array.isArray(text)) {
       // Map each string in the array to a <p> tag
@@ -26,20 +37,35 @@ export default function CtaTwoColumn({ title, text, buttonLabel, buttonLink }) {
   return (
     <div className={styles.boxWrap}>
       <div className={clsx("row", styles.row)}>
-        <div className={clsx("col col--7", styles.leftColumn)}>
-          {title && <h1 class="headingDot">{title}</h1>}
-         {text && renderText(text)}
+        <div className={clsx("col col--6", styles.leftColumn)}>
+          {leftTitle && <h1 className={clsx({ 'headingDot': leftHeadingDot })}>{leftTitle}</h1>}
+          {leftText && renderText(leftText)}
+          {leftButtonLabel && (
+            <Link
+              className={clsx(
+                "button button--primary button--lg",
+                styles.buttonWhite
+              )}
+              to={leftButtonLink}
+            >
+              {leftButtonLabel}
+            </Link>
+          )}
         </div>
-        <div className={clsx("col col--5", styles.rightColumn)}>
-          <Link
-            className={clsx(
-              "button button--primary button--lg",
-              styles.buttonWhite
-            )}
-            to={buttonLink}
-          >
-            {buttonLabel}
-          </Link>
+        <div className={clsx("col col--6", styles.rightColumn)}>
+          {rightTitle && <h1 className={clsx({ 'headingDot': rightHeadingDot })}>{rightTitle}</h1>}
+          {rightText && renderText(rightText)}
+          {rightButtonLabel && (
+            <Link
+              className={clsx(
+                "button button--primary button--lg",
+                styles.buttonWhite
+              )}
+              to={rightButtonLink}
+            >
+              {rightButtonLabel}
+            </Link>
+          )}
         </div>
       </div>
     </div>
