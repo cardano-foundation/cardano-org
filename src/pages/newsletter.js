a80de851ff8b734e6685e379e4b9a9b09b5541ae
Tommy Kammerer <tk@digitalsirup.com>
2024-03-01 23:29:04 +0100

    Add newsletter (Community Digest) hubspot form

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -4,6 +4,10 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import SiteHero from "@site/src/components/Layout/SiteHero";
 import BoundaryBox from "@site/src/components/Layout/BoundaryBox";
+import Divider from "@site/src/components/Layout/Divider";
+import TitleWithText from "@site/src/components/Layout/TitleWithText";
+import CommunityDigestForm from "@site/src/components/CommunityDigestFormHS";
+import SpacerBox from "@site/src/components/Layout/SpacerBox";
 
 function HomepageHeader() {
   const { siteTitle } = "useDocusaurusContext()";
@@ -11,7 +15,7 @@ function HomepageHeader() {
     <SiteHero
       title="Stay Informed"
       description="Get access to the latest Cardano news and content, and the hottest topics happening around the Cardano ecosystem."
-      bannerType="default"
+      bannerType="overlap"
     />
   );
 }
@@ -25,8 +29,17 @@ export default function Home() {
     >
       <HomepageHeader />
       <BoundaryBox>
-          FIXME: newsletter
+          <Divider text="Stay informed" />
+          <TitleWithText 
+              title="Cardano Community Digest" 
+              description={[
+                "Get your bi-weekly dose of the latest hot topics, development updates, a list of upcoming meetups, delegation strategy updates, what our Cardano Ambassadors are up to, and much more."
+              ]}
+              headingDot={true} 
+          />
+          <CommunityDigestForm />
         </BoundaryBox>
+        <SpacerBox size="medium" />
     </Layout>
   );
 }
