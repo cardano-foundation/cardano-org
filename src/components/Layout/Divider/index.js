3cecb2bf86c8c32c0e4402dff870b4524bfa41be
Tommy Kammerer <tk@digitalsirup.com>
2024-03-02 16:57:26 +0100

    Improve divider anchor scrolling for deep links to the site

--- src/components/Layout/Divider/index.js
+++ src/components/Layout/Divider/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import clsx from "clsx";
 import styles from "./styles.module.css";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
@@ -11,14 +11,25 @@ import useBaseUrl from "@docusaurus/useBaseUrl";
 // can use a id optional to link to a specific divider with /page#id
 
 export default function Divider({ text, id }) {
+  // This effect runs once after the initial render
+  useEffect(() => {
+    // Check if the URL's hash matches this divider's ID
+    if (window.location.hash === `#${id}`) {
+      // Use setTimeout to allow the page to render and layout changes to complete
+      setTimeout(() => {
+        document.getElementById(id)?.scrollIntoView();
+      }, 100);  
+    }
+  }, [id]); // Effect dependencies, re-run if id changes
+
   return (
     <div>
       {id && <div id={id} />}
       <br />
-        <div className={styles.header}>
-          <h6>{text}</h6>
-          <div className={styles.horizontalBar}></div>
-        </div>
+      <div className={styles.header}>
+        <h6>{text}</h6>
+        <div className={styles.horizontalBar}></div>
+      </div>
     </div>
   );
 }
