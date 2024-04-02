e1fcfc9cb53d3656123114fec35cfccc41fc7e85
Tommy Kammerer <tk@digitalsirup.com>
2024-04-02 08:39:17 +0200

    Revise copy of Emurgo

--- src/components/PartnersOverviewSection/index.js
+++ src/components/PartnersOverviewSection/index.js
@@ -39,16 +39,7 @@ const partnersData = {
         partners_item_image: "emurgo",
         partners_item_link: "https://emurgo.io",
         partners_item_body:
-          "A founding member of the Cardano protocol, EMURGO develops, supports, and incubates commercial opportunities and \
-          helps integrate businesses into our blockchain system. Essentially the for-profit arm of Cardano, EMURGO endeavors \
-          to advance the platform and drive adoption through commercial ventures. With offices and live projects across Singapore, \
-          Japan, the USA, India, and Indonesia, EMURGO has extensive expertise in blockchain R&D, plus a global network of related \
-          blockchain and industry partners.<br /><br />EMURGO is geared to support high-impact ideas with the potential to bring \
-          positive change across a range of sectors, especially financial services, supply chain, retail, healthcare, the public \
-          sector, and IoT. As a complete blockchain solutions provider EMURGO is dedicated to facilitating blockchain education – \
-          offering courses and expertise to universities, professionals, enterprises, and blockchain beginners. India is now home \
-          to the EMURGO Academy, and EMURGO Education students come from some of the world’s most esteemed companies: Accenture, \
-          Bank of America, IBM, Dell, Boeing, GE, Target Corporation, Morgan Stanley, S&P and more.",
+          "EMURGO is a blockchain technology company and a founding entity of the Cardano blockchain that provides products and services to drive the adoption of Cardano’s Web3 ecosystem. Established in 2015 in Japan, EMURGO's mission is to facilitate commercial adoption through dynamic partnerships with existing ecosystem members and the seamless integration of new entrants.<br /><br />By prioritizing investments, delivering continuous education, and providing infrastructure services, EMURGO aims to unlock the full potential of the Cardano ecosystem.",
       },
       {
         partners_item_label: "Input Output",
