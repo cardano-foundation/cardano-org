a4bd21ff40b96b27cacb799d7ed10c711601ba10
Tommy Kammerer <tk@digitalsirup.com>
2024-05-01 10:18:52 +0200

    Add open graph information for brand assets

--- src/pages/brand-assets.js
+++ src/pages/brand-assets.js
@@ -2,6 +2,7 @@ import Layout from "@theme/Layout";
 import SiteHero from "@site/src/components/Layout/SiteHero";
 import BoundaryBox from "@site/src/components/Layout/BoundaryBox";
 import BrandAssetsSection from "../components/BrandAssetsSection";
+import Head from '@docusaurus/Head';
 
 function HomepageHeader() {
   const { siteTitle } = "useDocusaurusContext()";
@@ -15,13 +16,23 @@ function HomepageHeader() {
   );
 }
 
+function MetaData() {
+  return (
+    <Head>
+      <meta property="og:image" content="https://cardano.org/img/og/brand-assets.jpg" />
+      <meta name="twitter:image" content="https://cardano.org/img/og/brand-assets.jpg" />
+    </Head>
+  )
+}
+
 export default function Home() {
   return (
     <Layout
-    title="Cardano - making the world work better for all"
-    description="An open platform designed to empower billions without economic identity by offering decentralized applications for managing identity, value, and governance."
+    title="Cardano - brand assets"
+    description="Our brand is a reflection of everything that we create. Here are some of the things that make up our brand and how you can use them."
     >
       <HomepageHeader />
+      <MetaData />
       <main>
         <BoundaryBox>
           <BrandAssetsSection />
