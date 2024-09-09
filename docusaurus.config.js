18fa2a720985ef6184073f76dcf590d7051492b0
katomm <tk@digitalsirup.com>
2024-09-09 18:24:08 +0200

    Replace link to explorers

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -96,7 +96,7 @@ const config = {
               {to: '/ouroboros', label: 'What is Ouroboros?'}, 
               {to: '/hardforks', label: 'Which hard forks were there?'}, 
               {to: '/genesis', label: 'About Genesis Distribution'}, 
-              {href: 'https://beta.explorer.cardano.org', label: 'Explore the blockchain'},
+              {href: 'https://explorer.cardano.org', label: 'Explore the Cardano blockchain'},
             ],
           },
           {
