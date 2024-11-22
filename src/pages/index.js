1708dffcedfdb9746b5e1b24b31b40fb628870f9
Tommy <tk@digitalsirup.com>
2024-11-23 00:37:16 +0100

    Replace enterprise page with use cases, remove all references to Atala Prism, Atala Scan, all other broken links

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,7 +52,7 @@ export default function Home() {
                 "Made Possible",
               ]}
               buttonLabel="Use Cases"
-              buttonLink="/enterprise"
+              buttonLink="/usecases"
               headingDot={true}
             />
 
