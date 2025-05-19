174c64d802acc2ffefa0ad6c8e86a1674c25940a
Tommy <tk@digitalsirup.com>
2025-05-19 21:18:01 +0200

    add header

--- src/components/Layout/WelcomeHero/index.js
+++ src/components/Layout/WelcomeHero/index.js
@@ -1,42 +1,47 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import clsx from "clsx";
 import styles from "./styles.module.css";
 import Link from "@docusaurus/Link";
 
 function WelcomeHero({ title, description }) {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "/img/headers/medusa.bundle.js";  
+    script.async = true;
+
+    
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}> 
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div id="medusa-root" ref={containerRef} className="medusa-container" />
+
       <div className="container">
         <div className={styles.taglineContainer}>
           <h1 className={clsx("hero__title", styles.heroTitle)}>{title}</h1>
-          <p className={clsx("hero__subtitle", styles.heroSubtitle)}>{description}</p>  
+          <p className={clsx("hero__subtitle", styles.heroSubtitle)}>{description}</p>
         </div>
+
         <div className={styles.cta}>
-          <Link
-            className={clsx("button button--primary button--lg", styles.button)}
-            to="/where-to-get-ada"
-          >
+          <Link className={clsx("button button--primary button--lg", styles.button)} to="/where-to-get-ada">
             Where to get ada?
           </Link>
-          <Link
-            className={clsx("button button--primary button--lg", styles.button)}
-            to="/developers"
-          >
+          <Link className={clsx("button button--primary button--lg", styles.button)} to="/developers">
             Start Building
           </Link>
         </div>
 
         <div className="sectionCaret">
           <svg x="0px" y="0px" viewBox="0 0 2000 30">
-            <polygon
-              className="polygon-fill"
-              points="1000,30 0,30 0,0 980,0 "
-            ></polygon>
-            <polygon
-              className="polygon-fill"
-              points="1000,30 2000,30 2000,0 1020,0 "
-            ></polygon>
+            <polygon className="polygon-fill" points="1000,30 0,30 0,0 980,0" />
+            <polygon className="polygon-fill" points="1000,30 2000,30 2000,0 1020,0" />
           </svg>
         </div>
       </div>
