7dbd326089302316bc1c810da27b39f1d68043f1
Individuo Digital <individuodigital@protonmail.com>
2026-01-04 20:34:59 -0300

    - Update the Ambassador list database by adding the new ambassadors and removing the off-boarded ones. - Update the link to the Luma Event calendar on the Ambassador website.

--- src/components/AmbassadorRolesSection/index.js
+++ src/components/AmbassadorRolesSection/index.js
@@ -28,7 +28,7 @@ const rolesData = [
   {
     title: "Meetups & Events",
     description: [
-      "Organizing both in-person and virtual gatherings, workshops, and hackathons to unite local communities. [Find your local community Meetup.](https://www.meetup.com/pro/cardano/)",
+      "Organizing both in-person and virtuanoal gatherings, workshops, and hackathons to unite local communities. [Find your local community events.](https://luma.com/CardanoEvents)",
     ],
   },
   {
