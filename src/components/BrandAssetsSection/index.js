b951fb6565348a433889edbfb444c62a047f9950
Tommy <tk@digitalsirup.com>
2026-03-19 16:14:05 +0100

    Revert "feat: expand brand-assets page with colors, typography, usage guidelines and SEO"
    This reverts commit 50028a82eb4cd231991528791a162d9fe7211485.

--- src/components/BrandAssetsSection/index.js
+++ src/components/BrandAssetsSection/index.js
@@ -1,121 +1,90 @@
 import clsx from "clsx";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import styles from "./styles.module.css";
-import Divider from "@site/src/components/Layout/Divider";
-import TitleWithText from "@site/src/components/Layout/TitleWithText";
+import Divider from "@site/src/components/Layout/Divider"; 
+import TitleWithText from "@site/src/components/Layout/TitleWithText"; 
 import { FaDownload } from "react-icons/fa6";
-import { translate } from "@docusaurus/Translate";
-
-const logoGroups = [
-  {
-    label: "Cardano Starburst Logo",
-    id: "starburst",
-    variants: [
-      { color: "white", file: "cardano-starburst-white.svg", bg: "dark" },
-      { color: "black", file: "cardano-starburst-black.svg", bg: "paper" },
-      { color: "blue", file: "cardano-starburst-blue.svg", bg: "light" },
-    ],
-  },
-  {
-    label: "Cardano Stacked Logo",
-    id: "stacked",
-    variants: [
-      { color: "white", file: "cardano-stacked-white.svg", bg: "dark" },
-      { color: "black", file: "cardano-stacked-black.svg", bg: "paper" },
-      { color: "blue", file: "cardano-stacked-blue.svg", bg: "light" },
-    ],
-  },
-  {
-    label: "Cardano Horizontal Logo (default)",
-    id: "default",
-    variants: [
-      { color: "white", file: "cardano-horizontal-white.svg", bg: "dark" },
-      { color: "black", file: "cardano-horizontal-black.svg", bg: "paper" },
-      { color: "blue", file: "cardano-horizontal-blue.svg", bg: "light" },
-    ],
-  },
-];
-
-function LogoCard({ variant, groupLabel }) {
-  const src = useBaseUrl(`img/brand-assets/${variant.file}`);
-  const bgClass =
-    variant.bg === "dark"
-      ? styles.cardBgDark
-      : variant.bg === "paper"
-        ? styles.cardBgPaper
-        : styles.cardBgLight;
-
-  return (
-    <div className={styles.logoCard}>
-      <div className={clsx(styles.logoCardPreview, bgClass)}>
-        <img
-          src={src}
-          alt={`${groupLabel} — ${variant.color} version`}
-          className={styles.logoCardImg}
-        />
-      </div>
-      <div className={styles.logoCardFooter}>
-        <span className={styles.logoCardLabel}>{variant.color}</span>
-        <a
-          href={src}
-          download={variant.file}
-          className={styles.logoCardDownload}
-          title={translate({
-            id: "brandAssets.logos.downloadSvg",
-            message: "Download SVG",
-          })}
-        >
-          <FaDownload />
-        </a>
-      </div>
-    </div>
-  );
-}
+ 
 
 export default function BrandAssetsSection() {
+
   return (
     <div>
-      {logoGroups.map((group) => (
-        <div key={group.id}>
-          <Divider text={group.label} id={group.id} />
-          <div className={styles.logoGrid}>
-            {group.variants.map((v) => (
-              <LogoCard key={v.file} variant={v} groupLabel={group.label} />
-            ))}
-          </div>
+        <Divider text="Download" id="download"/>
+        <a href="/downloads/cardano-brand-assets.zip">
+          <FaDownload className={styles.downloadIcon} />
+          <p>Download all Cardano brand assets from this page.</p></a>
+
+        <Divider text="Starburst" id="starburst"/>
+        <div className={styles.logoContainer}>
+          <img
+            src={useBaseUrl("img/brand-assets/cardano-starburst-white.svg")}
+            alt={"Cardano Starburst Logo White"}
+            className={clsx(styles.logo, styles.white)}
+          />
+          <img
+            src={useBaseUrl("img/brand-assets/cardano-starburst-black.svg")}
+            alt={"Cardano Starburst Logo Black"}
+            className={styles.logo}
+          />
+          <img
+            src={useBaseUrl("img/brand-assets/cardano-starburst-blue.svg")}
+            alt={"Cardano Starburst Logo Blue"}
+            className={styles.logo}
+          />
         </div>
-      ))}
 
-      <Divider
-        text={translate({ id: "brandAssets.logos.download", message: "Download" })}
-        id="download"
-      />
-      <a href="/downloads/cardano-brand-assets.zip" className={styles.downloadAll}>
-        <FaDownload className={styles.downloadIcon} />
-        <span>
-          {translate({
-            id: "brandAssets.logos.downloadAll",
-            message: "Download all Cardano brand assets from this page.",
-          })}
-        </span>
-      </a>
+        <Divider text="Stacked" id="stacked"/>
+        <div className={styles.logoContainer}>
+          <img
+            src={useBaseUrl("img/brand-assets/cardano-stacked-white.svg")}
+            alt={"Cardano Starburst Logo White"}
+            className={clsx(styles.logo, styles.white)}
+          />
+          <img
+            src={useBaseUrl("img/brand-assets/cardano-stacked-black.svg")}
+            alt={"Cardano Starburst Logo Black"}
+            className={styles.logo}
+          />
+          <img
+            src={useBaseUrl("img/brand-assets/cardano-stacked-blue.svg")}
+            alt={"Cardano Starburst Logo Blue"}
+            className={styles.logo}
+          />
+        </div>
 
-      <TitleWithText
-        description={[
-          translate({
-            id: "brandAssets.logos.desc1",
-            message:
-              "Our logo is at the heart of the brand identity. While it is the most recognizable part of the brand identity it is not the only component. The horizontal logo (shown here) is the primary version and should be used as the default version.",
-          }),
-          translate({
-            id: "brandAssets.logos.desc2",
-            message:
-              "Please note the [Cardano Foundation Trademark Policy](https://cardanofoundation.org/en/trademark-policy).",
-          }),
-        ]}
-        titleType="black"
-        headingDot={true}
-      />
+        <Divider text="Horizontal (default)" id="default"/>
+        <div className={styles.logoContainer}>
+          <img
+            src={useBaseUrl("img/brand-assets/cardano-horizontal-white.svg")}
+            alt={"Cardano Logo White"}
+            className={clsx(styles.logo, styles.white)}
+          />
+          <img
+            src={useBaseUrl("img/brand-assets/cardano-horizontal-black.svg")}
+            alt={"Cardano Logo Black"}
+            className={styles.logo}
+          />
+          <img
+            src={useBaseUrl("img/brand-assets/cardano-horizontal-blue.svg")}
+            alt={"Cardano Logo Blue"}
+            className={styles.logo}
+          />
+        </div>
+      
+        <TitleWithText
+              
+              description={[
+                "Our logo is at the heart of the brand identity. While it is the most recognizable part of the brand \
+                identity it is not the only component. The horizontal logo (shown here) is the primary version and \
+                should be used as the default version.",
+
+                "Please note the [Cardano Foundation Trademark Policy](https://cardanofoundation.org/en/trademark-policy).",
+              ]}
+              titleType="black"
+              headingDot={true}
+            />
     </div>
   );
-}
+  }
+
