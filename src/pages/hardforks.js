d99870ebeea44d13b8a450bf34c0925819f97d2a
gufmar <gufmar@gmail.com>
2026-05-20 12:55:10 +0200

    add van Rossem Hardfork (tentative)

--- src/pages/hardforks.js
+++ src/pages/hardforks.js
@@ -11,7 +11,7 @@ import OpenGraphInfo from "@site/src/components/Layout/OpenGraphInfo";
 import SpacerBox from "@site/src/components/Layout/SpacerBox";
 import { Chrono } from "react-chrono";
 import BrowserOnly from '@docusaurus/BrowserOnly';
-import {translate} from '@docusaurus/Translate';
+import { translate } from '@docusaurus/Translate';
 
 // Hard Fork List
 function getTimelineItems() {
@@ -21,10 +21,10 @@ function getTimelineItems() {
       cardTitle: "September 29, 2017",
       cardSubtitle: "Name: Byron",
       cardDetailedText: [
-        translate({id: 'hardforks.timeline.byron.description', message: 'The launch of the Cardano mainnet and introduction of the ada cryptocurrency.'}),
-        translate({id: 'hardforks.timeline.label.epoch', message: 'Epoch'}) + " 0",
-        translate({id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version'}) + ": 1.0",
-        translate({id: 'hardforks.timeline.label.transactionId', message: 'Transaction ID'}) + ": N/A (Genesis block)",
+        translate({ id: 'hardforks.timeline.byron.description', message: 'The launch of the Cardano mainnet and introduction of the ada cryptocurrency.' }),
+        translate({ id: 'hardforks.timeline.label.epoch', message: 'Epoch' }) + " 0",
+        translate({ id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version' }) + ": 1.0",
+        translate({ id: 'hardforks.timeline.label.transactionId', message: 'Transaction ID' }) + ": N/A (Genesis block)",
       ],
     },
     {
@@ -32,10 +32,10 @@ function getTimelineItems() {
       cardTitle: "July 29, 2020",
       cardSubtitle: "Name: Shelley",
       cardDetailedText: [
-        translate({id: 'hardforks.timeline.shelley.description', message: 'Introduced staking and decentralization features, transitioning from a federated to a decentralized system.'}),
-        translate({id: 'hardforks.timeline.label.epoch', message: 'Epoch'}) + " 208",
-        translate({id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version'}) + ": 2.0",
-        translate({id: 'hardforks.timeline.label.transactionId', message: 'Transaction ID'}) + ": N/A (Transitioned through several updates)",
+        translate({ id: 'hardforks.timeline.shelley.description', message: 'Introduced staking and decentralization features, transitioning from a federated to a decentralized system.' }),
+        translate({ id: 'hardforks.timeline.label.epoch', message: 'Epoch' }) + " 208",
+        translate({ id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version' }) + ": 2.0",
+        translate({ id: 'hardforks.timeline.label.transactionId', message: 'Transaction ID' }) + ": N/A (Transitioned through several updates)",
       ],
     },
     {
@@ -43,10 +43,10 @@ function getTimelineItems() {
       cardTitle: "December 16, 2020",
       cardSubtitle: "Name: Allegra",
       cardDetailedText: [
-        translate({id: 'hardforks.timeline.allegra.description', message: 'Added token locking capabilities, which was a prerequisite for the smart contract functionality.'}),
-        translate({id: 'hardforks.timeline.label.epoch', message: 'Epoch'}) + " 236",
-        translate({id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version'}) + ": 3.0",
-        translate({id: 'hardforks.timeline.label.transactionId', message: 'Transaction ID'}) + ": 1fbd16c1d1b1933f2f97a313db8c749bdcf65a39d996515b0f5e5535baad68e8",
+        translate({ id: 'hardforks.timeline.allegra.description', message: 'Added token locking capabilities, which was a prerequisite for the smart contract functionality.' }),
+        translate({ id: 'hardforks.timeline.label.epoch', message: 'Epoch' }) + " 236",
+        translate({ id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version' }) + ": 3.0",
+        translate({ id: 'hardforks.timeline.label.transactionId', message: 'Transaction ID' }) + ": 1fbd16c1d1b1933f2f97a313db8c749bdcf65a39d996515b0f5e5535baad68e8",
       ],
     },
     {
@@ -54,10 +54,10 @@ function getTimelineItems() {
       cardTitle: "March 1, 2021",
       cardSubtitle: "Name: Mary",
       cardDetailedText: [
-        translate({id: 'hardforks.timeline.mary.description', message: 'Brought native token functionality to Cardano, allowing users to create and transact with custom tokens.'}),
-        translate({id: 'hardforks.timeline.label.epoch', message: 'Epoch'}) + " 251",
-        translate({id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version'}) + ": 4.0",
-        translate({id: 'hardforks.timeline.label.transactionId', message: 'Transaction ID'}) + ": b7f5658a5aabced7f8599cf7bf7cb9d6f730b865a5a0430f2dc7488caf25752e",
+        translate({ id: 'hardforks.timeline.mary.description', message: 'Brought native token functionality to Cardano, allowing users to create and transact with custom tokens.' }),
+        translate({ id: 'hardforks.timeline.label.epoch', message: 'Epoch' }) + " 251",
+        translate({ id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version' }) + ": 4.0",
+        translate({ id: 'hardforks.timeline.label.transactionId', message: 'Transaction ID' }) + ": b7f5658a5aabced7f8599cf7bf7cb9d6f730b865a5a0430f2dc7488caf25752e",
       ],
     },
     {
@@ -65,10 +65,10 @@ function getTimelineItems() {
       cardTitle: "September 12, 2021",
       cardSubtitle: "Name: Alonzo",
       cardDetailedText: [
-        translate({id: 'hardforks.timeline.alonzo.description', message: 'Introduced smart contract capabilities using Plutus, enabling the deployment of decentralized applications (dApps).'}),
-        translate({id: 'hardforks.timeline.label.epoch', message: 'Epoch'}) + " 290",
-        translate({id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version'}) + ": 5.0",
-        translate({id: 'hardforks.timeline.label.transactionId', message: 'Transaction ID'}) + ": N/A (Specific transaction ID not provided)",
+        translate({ id: 'hardforks.timeline.alonzo.description', message: 'Introduced smart contract capabilities using Plutus, enabling the deployment of decentralized applications (dApps).' }),
+        translate({ id: 'hardforks.timeline.label.epoch', message: 'Epoch' }) + " 290",
+        translate({ id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version' }) + ": 5.0",
+        translate({ id: 'hardforks.timeline.label.transactionId', message: 'Transaction ID' }) + ": N/A (Specific transaction ID not provided)",
       ],
     },
     {
@@ -77,9 +77,9 @@ function getTimelineItems() {
       cardSubtitle: "Name: (Lobster)", //TODO: confirm name
       cardDetailedText: [
         // "-", // FIXME: what changed?
-        translate({id: 'hardforks.timeline.label.epoch', message: 'Epoch'}) + " 298",
-        translate({id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version'}) + ": 6.0",
-        translate({id: 'hardforks.timeline.label.transactionId', message: 'Transaction ID'}) + ": N/A (Specific transaction ID not provided)",
+        translate({ id: 'hardforks.timeline.label.epoch', message: 'Epoch' }) + " 298",
+        translate({ id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version' }) + ": 6.0",
+        translate({ id: 'hardforks.timeline.label.transactionId', message: 'Transaction ID' }) + ": N/A (Specific transaction ID not provided)",
       ],
     },
     {
@@ -87,10 +87,10 @@ function getTimelineItems() {
       cardTitle: "September 22, 2022",
       cardSubtitle: "Name: Vasil",
       cardDetailedText: [
-        translate({id: 'hardforks.timeline.vasil.description', message: 'Improved the scalability and performance of the network, named after Vasil Dabov, a Cardano community member.'}),
-        translate({id: 'hardforks.timeline.label.epoch', message: 'Epoch'}) + " 365",
-        translate({id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version'}) + ": 7.0",
-        translate({id: 'hardforks.timeline.label.transactionIds', message: 'Transaction IDs'}) + ": 3abda97c78c71e8a21473529aca94d78d364dfa1a866ef8245885e18085b4e4c, 8230f33cd7ad3f8601e94ea2b18abdc591187e190ea8ebecc25e20fc66200f13",
+        translate({ id: 'hardforks.timeline.vasil.description', message: 'Improved the scalability and performance of the network, named after Vasil Dabov, a Cardano community member.' }),
+        translate({ id: 'hardforks.timeline.label.epoch', message: 'Epoch' }) + " 365",
+        translate({ id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version' }) + ": 7.0",
+        translate({ id: 'hardforks.timeline.label.transactionIds', message: 'Transaction IDs' }) + ": 3abda97c78c71e8a21473529aca94d78d364dfa1a866ef8245885e18085b4e4c, 8230f33cd7ad3f8601e94ea2b18abdc591187e190ea8ebecc25e20fc66200f13",
       ],
     },
     {
@@ -98,10 +98,10 @@ function getTimelineItems() {
       cardTitle: "February 14, 2023",
       cardSubtitle: "Name: Valentine",
       cardDetailedText: [
-        translate({id: 'hardforks.timeline.valentine.description', message: 'Introduced further improvements to Plutus smart contract functionality and overall network performance.'}),
-        translate({id: 'hardforks.timeline.label.epoch', message: 'Epoch'}) + " 394",
-        translate({id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version'}) + ": 8.0",
-        translate({id: 'hardforks.timeline.label.transactionIds', message: 'Transaction IDs'}) + ": a83f479c5635e1e563a19f6e72a1be59fb082bbf31de90cc176850ee799b08ac, 62c3c13187423c47f629e6187f36fbd61a9ba1d05d101588340cfbfdf47b22d2",
+        translate({ id: 'hardforks.timeline.valentine.description', message: 'Introduced further improvements to Plutus smart contract functionality and overall network performance.' }),
+        translate({ id: 'hardforks.timeline.label.epoch', message: 'Epoch' }) + " 394",
+        translate({ id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version' }) + ": 8.0",
+        translate({ id: 'hardforks.timeline.label.transactionIds', message: 'Transaction IDs' }) + ": a83f479c5635e1e563a19f6e72a1be59fb082bbf31de90cc176850ee799b08ac, 62c3c13187423c47f629e6187f36fbd61a9ba1d05d101588340cfbfdf47b22d2",
       ],
     },
     {
@@ -109,10 +109,10 @@ function getTimelineItems() {
       cardTitle: "September 1, 2024",
       cardSubtitle: "Name: Chang 1",
       cardDetailedText: [
-        translate({id: 'hardforks.timeline.chang1.description', message: 'Introducing the first batch of decentralized governance features of CIP-1694. Enabling only parameter changes and hard fork initiations.'}),
-        translate({id: 'hardforks.timeline.label.epoch', message: 'Epoch'}) + " 507",
-        translate({id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version'}) + ": 9.0",
-        translate({id: 'hardforks.timeline.label.transactionIds', message: 'Transaction IDs'}) + ": 9ba6a580bceb8f94e65a683e8291c89382835f46e3cf928eb521f5581ade4820, 4e377ceb5c5721257a3d7960f3053468bbea45ed8ac22cd559c69e757da5e0ae",
+        translate({ id: 'hardforks.timeline.chang1.description', message: 'Introducing the first batch of decentralized governance features of CIP-1694. Enabling only parameter changes and hard fork initiations.' }),
+        translate({ id: 'hardforks.timeline.label.epoch', message: 'Epoch' }) + " 507",
+        translate({ id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version' }) + ": 9.0",
+        translate({ id: 'hardforks.timeline.label.transactionIds', message: 'Transaction IDs' }) + ": 9ba6a580bceb8f94e65a683e8291c89382835f46e3cf928eb521f5581ade4820, 4e377ceb5c5721257a3d7960f3053468bbea45ed8ac22cd559c69e757da5e0ae",
       ],
     },
     {
@@ -120,10 +120,21 @@ function getTimelineItems() {
       cardTitle: "January 29, 2025",
       cardSubtitle: "Name: Plomin",
       cardDetailedText: [
-        translate({id: 'hardforks.timeline.plomin.description', message: 'Introducing the second batch of decentralized governance features of CIP-1694. Enabling the full set of governance actions and the DRep role.'}),
-        translate({id: 'hardforks.timeline.label.epoch', message: 'Epoch'}) + " 537",
-        translate({id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version'}) + ": 10.0",
-        translate({id: 'hardforks.timeline.label.transactionIds', message: 'Transaction IDs'}) + ": 0b19476e40bbbb5e1e8ce153523762e2b6859e7ecacbaf06eae0ee6a447e79b9",
+        translate({ id: 'hardforks.timeline.plomin.description', message: 'Introducing the second batch of decentralized governance features of CIP-1694. Enabling the full set of governance actions and the DRep role.' }),
+        translate({ id: 'hardforks.timeline.label.epoch', message: 'Epoch' }) + " 537",
+        translate({ id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version' }) + ": 10.0",
+        translate({ id: 'hardforks.timeline.label.transactionIds', message: 'Transaction IDs' }) + ": 0b19476e40bbbb5e1e8ce153523762e2b6859e7ecacbaf06eae0ee6a447e79b9",
+      ],
+    },
+    {
+      title: "Conway Era",
+      cardTitle: "June, 2026 (tentative)",
+      cardSubtitle: "Name: van Rossem",
+      cardDetailedText: [
+        translate({ id: 'hardforks.timeline.vanrossem.description', message: 'Inter-era upgrade delivering cleaner ledger rules, VRF key uniqueness, Plutus smart contract performance improvements and new cryptographic built-ins' }),
+        translate({ id: 'hardforks.timeline.label.epoch', message: 'Epoch' }) + " tbd",
+        translate({ id: 'hardforks.timeline.label.protocolVersion', message: 'Protocol Version' }) + ": 11.0",
+        translate({ id: 'hardforks.timeline.label.transactionIds', message: 'Transaction IDs' }) + ": tbd",
       ],
     },
   ];
@@ -132,8 +143,8 @@ function getTimelineItems() {
 function HomepageHeader() {
   return (
     <SiteHero
-      title={translate({id: 'hardforks.hero.title', message: 'Hard forks'})}
-      description={translate({id: 'hardforks.hero.description', message: 'What hard forks were implemented, and what functionalities did they introduce?'})}
+      title={translate({ id: 'hardforks.hero.title', message: 'Hard forks' })}
+      description={translate({ id: 'hardforks.hero.description', message: 'What hard forks were implemented, and what functionalities did they introduce?' })}
       bannerType="starburst"
     />
   );
@@ -142,21 +153,21 @@ function HomepageHeader() {
 export default function Home() {
   return (
     <Layout
-      title={translate({id: 'hardforks.layout.title', message: 'Cardano Hard Forks, Network Upgrade History'})}
-      description={translate({id: 'hardforks.layout.description', message: 'A complete timeline of Cardano hard forks from Byron to Chang. Explore each network upgrade, what changed, and how the protocol has evolved.'})}
+      title={translate({ id: 'hardforks.layout.title', message: 'Cardano Hard Forks, Network Upgrade History' })}
+      description={translate({ id: 'hardforks.layout.description', message: 'A complete timeline of Cardano hard forks from Byron to Chang. Explore each network upgrade, what changed, and how the protocol has evolved.' })}
     >
       <OpenGraphInfo pageName="hard-forks" />
       <HomepageHeader />
       <BackgroundWrapper backgroundType={"zoom"}>
         <BoundaryBox>
           <TitleWithText
-            title={translate({id: 'hardforks.content.title', message: 'Cardano Hard Forks'})}
-            description={translate({id: 'hardforks.content.description', message: 'Hard forks in Cardano do not signify division and differences within the ecosystem. On the contrary, they define a specific and collectively agreed-upon exact time (slot) when all nodes switch from the current era to a new one, applying new functions, validation rules, or parameter values. All stake pool operators need to install the upgrade, and they also have a say and must agree to it. A Cardano hard fork is, therefore, not a separation but a precise collective evolution.'})}
+            title={translate({ id: 'hardforks.content.title', message: 'Cardano Hard Forks' })}
+            description={translate({ id: 'hardforks.content.description', message: 'Hard forks in Cardano do not signify division and differences within the ecosystem. On the contrary, they define a specific and collectively agreed-upon exact time (slot) when all nodes switch from the current era to a new one, applying new functions, validation rules, or parameter values. All stake pool operators need to install the upgrade, and they also have a say and must agree to it. A Cardano hard fork is, therefore, not a separation but a precise collective evolution.' })}
             headingDot={true}
           />
-          <Divider text={translate({id: 'hardforks.divider.timeline', message: 'Timeline'})} id="timeline"/>
+          <Divider text={translate({ id: 'hardforks.divider.timeline', message: 'Timeline' })} id="timeline" />
 
-          <BrowserOnly fallback={<div>{translate({id: 'hardforks.loading', message: 'Loading...'})}</div>}>
+          <BrowserOnly fallback={<div>{translate({ id: 'hardforks.loading', message: 'Loading...' })}</div>}>
             {() => (
               <Chrono
                 items={getTimelineItems()}
@@ -182,11 +193,11 @@ export default function Home() {
 
       <BackgroundWrapper backgroundType={"solidGrey"}>
         <BoundaryBox>
-          <Divider text={translate({id: 'hardforks.divider.transactionIds', message: 'hard fork transaction ids'})} />
+          <Divider text={translate({ id: 'hardforks.divider.transactionIds', message: 'hard fork transaction ids' })} />
           <TitleWithText
-            description={translate({id: 'hardforks.transactionIds.description', message: 'Note that some hard forks, particularly Byron and Shelley, transitioned through a series of updates and may not have a single specific transaction id associated with them. For the others, the provided transaction ids correspond to significant parameter updates leading to the hard forks.'})}
+            description={translate({ id: 'hardforks.transactionIds.description', message: 'Note that some hard forks, particularly Byron and Shelley, transitioned through a series of updates and may not have a single specific transaction id associated with them. For the others, the provided transaction ids correspond to significant parameter updates leading to the hard forks.' })}
           />
-          <SpacerBox size="medium"/>
+          <SpacerBox size="medium" />
         </BoundaryBox>
       </BackgroundWrapper>
     </Layout>
